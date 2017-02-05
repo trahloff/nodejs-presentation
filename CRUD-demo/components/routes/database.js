990d2805074b0ca9014505cea1201d5a67ba951c
Tobias Rahloff <rahloff@gmail.com>
2017-02-05 21:31:43 +0100

    semicolons

--- CRUD-demo/components/routes/database.js
+++ CRUD-demo/components/routes/database.js
@@ -10,13 +10,13 @@ api
         db.get(request.params.key, (error, value) => {
             let answer = error ? error.toString() : value;
             response.send(answer);
-        })
+        });
     })
     .put("/:key/:value", (request, response) => {
         db.put(request.params.key, request.params.value, (error) => {
             let answer = error ? error.toString() : 'key: "' + request.params.key + '" now has value: "' + request.params.value + '"';
             response.send(answer);
-        })
+        });
     })
     .delete("/:key", (request, response) => {
         db.del(request.params.key, (error) => {
