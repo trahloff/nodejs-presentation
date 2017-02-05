e1c9c221798e0c79c9498a74011bc5b1267ca706
Tobias Rahloff <rahloff@gmail.com>
2017-02-05 21:39:25 +0100

    simplified stuff

--- ecma-demos/arrow_functions.js
+++ ecma-demos/arrow_functions.js
@@ -3,7 +3,3 @@ this.hello = 'world';
 setTimeout(function() {
     console.log('function notation: Hello ' + this.hello);
 }, 500);
-
-setTimeout(() => {
-    console.log('arrow notation: Hello ' + this.hello);
-}, 500);
--- ecma-demos/varLet.js
+++ ecma-demos/varLet.js
@@ -1,20 +1,6 @@
-function varTest() {
-    var x = 1;
-    if (true) {
-        var x = 2;
-        console.log('var  innerhalb if: ' + x);
-    }
-    console.log('var  außerhalb if: ' + x);
+let x = 1;
+if (true) {
+    let x = 2;
+    console.log('let  innerhalb if: ' + x);
 }
-
-function letTest() {
-    let x = 1;
-    if (true) {
-        let x = 2;
-        console.log('let innerhalb if: ' + x);
-    }
-    console.log('let außerhalb if: ' + x);
-}
-
-varTest();
-letTest();
+console.log('let  außerhalb if: ' + x);
